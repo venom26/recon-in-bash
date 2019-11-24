e2a0664a3172fddabf3e0f52ad7ec5197803bf5e
venom26 <54362015+venom26@users.noreply.github.com>
2019-11-24 01:29:08 -0600

    Update 1_enum.sh

--- 1_enum.sh
+++ 1_enum.sh
@@ -1,13 +1,14 @@
 ##!/bin/bash
 #starting sublist3r
-sublist3r -d $1 -v -o domains.txt
+CUR_DIR=$(pwd)
+sublist3r -d $1 -v -o $CUR_DIR/domains.txt
 #running assetfinder
-assetfinder --subs-only $1 | tee -a domains.txt
+assetfinder --subs-only $1 | tee -a $CUR_DIR/domains.txt
 #removing duplicate entries
-sort -u domains.txt -o domains.txt
+sort -u domains.txt -o $CUR_DIR/domains.txt
 #checking for alive domains
 echo "\n\n[+] Checking for alive domains..\n"
-cat domains.txt | httprobe | tee -a alive.txt
+cat domains.txt | httprobe | tee -a $CUR_DIR/alive.txt
 #formatting the data to json
-cat alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > alive.json
-cat domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > domains.json
+cat $CUR_DIR/alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/alive.json
+cat $CUR_DIR/domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/domains.json
