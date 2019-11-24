5f26bfbdc534ccb54d2f8364ee60354ce4a5fab8
venom26 <54362015+venom26@users.noreply.github.com>
2019-11-24 01:42:39 -0600

    Update 4_endpoints.sh

--- 4_endpoints.sh
+++ 4_endpoints.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #looping through the scriptsresponse directory
-mkdir endpoints
 CUR_DIR=$(pwd)
-for domain in $(ls scriptsresponse)
+mkdir $CUR_DIR/endpoints
+
+for domain in $(ls $CUR_DIR/scriptsresponse)
 do
         #looping through files in each domain
-        mkdir endpoints/$domain
-        for file in $(ls scriptsresponse/$domain)
+        mkdir $CUR_DIR/endpoints/$domain
+        for file in $(ls $CUR_DIR/scriptsresponse/$domain)
         do
-                ruby /home/venom/tools/relative-url-extractor/extract.rb scriptsresponse/$domain/$file >> endpoints/$domain/$file 
+                ruby /home/venom/tools/relative-url-extractor/extract.rb $CUR_DIR/scriptsresponse/$domain/$file >> $CUR_DIR/endpoints/$domain/$file 
         done
 done
