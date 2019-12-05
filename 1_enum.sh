23cb484b194236461312ce7a00b88e59bbdd8023
venom26 <54362015+venom26@users.noreply.github.com>
2019-12-05 12:09:19 -0600

    Update 1_enum.sh

--- 1_enum.sh
+++ 1_enum.sh
@@ -1,13 +1,21 @@
-##!/bin/bash
+#!/bin/bash
 #starting sublist3r
 CUR_DIR=$(pwd)
 sublist3r -d $1 -v -o $CUR_DIR/domains.txt
+
 #running assetfinder
 assetfinder --subs-only $1 | tee -a $CUR_DIR/domains.txt
+
+#running bufferover
+curl -ss https://dns.bufferover.run/dns?q=.$1 | jq '.FDNS_A[]' | sed 's/^\".*.,//g' | sed 's/\"$//g'  | sort -u | tee -a $CUR_DIR/domains.txt
+
+#running certspotter
+curl -ss https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | tee -a $CUR_DIR/domains.txt
+
 #removing duplicate entries
 sort -u domains.txt -o $CUR_DIR/domains.txt
 #checking for alive domains
-echo "\n\n[+] Checking for alive domains..\n"
+echo "Checking for alive domains"
 cat domains.txt | httprobe | tee -a $CUR_DIR/alive.txt
 #formatting the data to json
 cat $CUR_DIR/alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/alive.json
